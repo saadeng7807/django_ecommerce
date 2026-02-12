d0306f97680f7825683892d0c3040e19f31c5a8e
saadeng7807 <tuwaiq68@gmail.com>
2026-02-12 20:44:02 +0300

    Model

--- category/views.py
+++ category/views.py
@@ -1,18 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Category
 # Create your views here.
 
-def index(request):
+# def index(request):
     
-    categories = [
-        {"id": 1, "name": "Electronics"},
-        {"id": 2, "name": "Clothes"},
-        {"id": 3, "name": "Books"},
-        {"id": 4, "name": "Sports"},
-        {"id": 5, "name": "Home"},
-    ]
+#     categories = [
+#         {"id": 1, "name": "Electronics"},
+#         {"id": 2, "name": "Clothes"},
+#         {"id": 3, "name": "Books"},
+#         {"id": 4, "name": "Sports"},
+#         {"id": 5, "name": "Home"},
+#     ]
 
+#     context={
+#         'cat':categories
+#     }
+
+#     return render(request,'category/index.html',context)
+
+def index(request):
+
+    categories=Category.objects.all()
+    print(categories)
     context={
         'cat':categories
     }
--- homeproject/settings.py
+++ homeproject/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'homeproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ecommerce',
+        'USER':'root',
+        'PASSWORD':'12345677',
+        'HOST':'localhost',
+        'PORT':'3306'
+
     }
 }
 
