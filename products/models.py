6172c77263fcbc3a793acacebc0e72f972650ae5
saadeng7807 <tuwaiq68@gmail.com>
2026-02-12 21:08:56 +0300

    Model2

--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from category.models import Category
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name=models.CharField(max_length=50)
+    price=models.DecimalField(max_digits=10,decimal_places=5)
+    image_url=models.URLField(max_length=500)
+    Category=models.ForeignKey(Category,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+
+   
